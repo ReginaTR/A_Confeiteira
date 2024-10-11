e334955161ef3b6213fe1000529b97e4ab202387
ReginaTR <regininhar@hotmail.com>
2024-10-11 16:20:55 -0300

    Config React

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "propshaft", "~> 1.1"
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "react", to: "https://ga.jspm.io/npm:react@18.0.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.0.0/index.js"
+
